c10f817727ffdfeab10f228f82b0a656cbfbcffe
mthorry <mthorry@gmail.com>
2017-07-16 18:23:09 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  arr.sort_by do |phrase| #sorts each phrase in array
+    phrase.chars.map { |letter| alphabet.index(letter) } #creates an array of each letter in the phrase with the index of the alphabet. This is used to put the phrases in the correct order
+  end
+
+  # binding.pry
 end
